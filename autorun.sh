1fa2544d24f35c062eb9f61f37b5ebef7fae2bf2
zhouhua <zhouhua_8@163.com>
2021-05-27 20:46:18 +0800

    Update autorun.sh
    修改项目名

--- autorun.sh
+++ autorun.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-cd /home/pi/Python-aliddns_ipv4-ipv6
+cd /home/pi/Python-aliddns
 while true
 do
-	/usr/bin/python3 /home/pi/Python-aliddns_ipv4-ipv6/ddns.py
+	/usr/bin/python3 /home/pi/Python-aliddns/ddns.py
 	sleep 3600
 done
